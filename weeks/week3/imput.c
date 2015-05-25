db9c358faddb982d1482862bfa573ec1e2f66fdf
Pablo-r4001 <pablolafongarc@gmail.com>
2015-05-25 10:52:02 -0600

    modified:   imput.c

--- weeks/week3/imput.c
+++ weeks/week3/imput.c
@@ -1,18 +1,28 @@
+
+// Standard imput output
 #include <stdio.h>
 
+// Define constant
 #define STUDENTS 3
 
 int main(void)
 {
+	// scores to be added
 	int scores[] = {96, 90, 83 };
+
+	// to database file (overwrite)
 	FILE* file = fopen("database", "w");
 	
+	// if the file was succesfully created
 	if (file != NULL)
 	{
+		// add each score in a new line.
 		for(int i = 0; i < STUDENTS; i++)
 		{
+			to the file
 			fprintf(file, "%i\n", scores[i]);
 		}
 	}
+	// and close it
 	fclose(file);
 }
